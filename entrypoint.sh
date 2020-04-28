7fe692e4c4a66996a38ee2459fda7d33b1f2750f
Oliver Nybroe <olivernybroe@gmail.com>
2020-04-28 12:40:23 +0200

    fix(action): Set default output dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,10 @@ if [ -z $INPUT_MEMORY_LIMIT ]; then
   INPUT_MEMORY_LIMIT="128M"
 fi
 
+if [ -z $INPUT_OUTPUT_DIR ]; then
+  $INPUT_OUTPUT_DIR="docs"
+fi
+
 php -d memory_limit=$INPUT_MEMORY_LIMIT /generator/codingStyleGenerator.phar $INPUT_OUTPUT_DIR $*
 
 vuepress build $INPUT_OUTPUT_DIR
